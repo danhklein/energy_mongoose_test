c251ec65ec2a92ba37e0a26a06cfeea202149622
Daniel Klein <danhirschklein@gmail.com>
2016-05-30 18:25:02 -0600

    Deleted superfluous models

--- server/models/producers.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var Producer = new Schema ({
-    type : String,
-    source : [{type : Schema.Types.ObjectId, ref : 'sources'}]
-});
-
-module.exports = mongoose.model('producers', Producer);
--- server/models/sources.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var Source = new Schema ({
-    source : String,
-    generation : [{type : Schema.Types.ObjectId, ref : 'generations'}]
-});
-
-module.exports = mongoose.model('sources', Source);
